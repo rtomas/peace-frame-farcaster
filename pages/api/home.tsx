8d83888958cae692f96b2d558eca4850e2864fed
Tomas Rawski <rtomas@gmail.com>
2024-03-01 01:05:06 -0300

    Update home.tsx

--- pages/api/home.tsx
+++ pages/api/home.tsx
@@ -13,12 +13,12 @@ export default async function handler(req: NextRequest) {
         <meta property="og:title" content="Colors for the peace" />
         <meta property="og:image" content="https://peace-frame-farcaster.vercel.app/paz.jpg" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://peace-frame-farcaster.vercel.app/api/peace-v3?${Math.random()}" />
         <meta property="fc:frame:button:1" content="More colors..." />
         <meta property="fc:frame:button:2" content="Colors for the peace" />
         <meta property="fc:frame:button:2:action" content="link" />
         <meta property="fc:frame:button:2:target" content="https://www.tomasrawski.com.ar/projects/peace-colors.html" />
         <meta property="fc:frame:post_url" content="https://peace-frame-farcaster.vercel.app/api/home" />
+        <meta property="fc:frame:image" content="https://peace-frame-farcaster.vercel.app/api/peace-v3?${Math.random()}" />
         </head>
         <body>
         <image src="https://peace-frame-farcaster.vercel.app/api/peace-v3?${Math.random()}">
